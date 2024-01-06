4bea53044d46d692e7435bf34ae3c765987c6580
Jaehoon-So <jhspacelover@naver.com>
2024-01-07 06:47:51 +0900

    add: item info html

---
+++ src/main/java/mangmae/gongnomok/HomeController.java
@@ -0,0 +1,16 @@
+package mangmae.gongnomok;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+@Controller
+public class HomeController {
+
+//    @GetMapping
+//    public String home() {
+//        return ""
+//    }
+
+}
