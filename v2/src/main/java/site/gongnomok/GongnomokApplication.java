06d458fa42efc877691d7e696960701552823572
Jaehoon-So <jhspacelover@naver.com>
2024-02-09 18:25:00 +0900

    add: version 2

---
+++ v2/src/main/java/site/gongnomok/GongnomokApplication.java
@@ -0,0 +1,13 @@
+package site.gongnomok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongnomokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongnomokApplication.class, args);
+	}
+
+}
