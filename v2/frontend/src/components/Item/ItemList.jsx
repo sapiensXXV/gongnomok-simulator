6dc05a507ab62d5fc97ae9033582bbd1cad69713
Jaehoon-So <jhspacelover@naver.com>
2024-02-22 19:01:58 +0900

    feat: 더보기 버튼

--- v2/frontend/src/components/Item/ItemList.jsx
+++ v2/frontend/src/components/Item/ItemList.jsx
@@ -1,23 +1,26 @@
+import { forwardRef, useEffect, useState } from "react";
 import SingleItem from "./SingleItem";
 
 
 
-export default function ItemList({ itemList, isItemLoaded }) {
+export default function ItemList({ 
+  itemList, 
+  isItemLoaded,
+  loadingHandler,
+  hasNextPage
+}) {
+
+  const loadingOption = {
+    root: null,
+    rootMargin: '0px',
+    thresgold: 1.0
+  };
 
   return (
     <>
       
       <section className="bg-light rounded py-2 px-1">
         <h2 className="item-list-title text-center">아이템 목록</h2>
-
-        { !isItemLoaded &&
-          <div className="text-center">
-            <div className="spinner-border" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </div>
-          </div>
-        } 
-
         <section className="col-md-12 bg-light rounded item-list-container px-2 py-2">
           <div className="row row-cols-xs-3 row-cols-sm-4 row-cols-md-6 row-cols-lg-auto row-cols-xl-6 g-3 d-flex justify-content-start">
 
@@ -31,7 +34,25 @@ export default function ItemList({ itemList, isItemLoaded }) {
               })
             }
           </div>
+          {
+            hasNextPage && 
+              <button className="btn btn-light more-item-button">
+                더보기
+              </button>
+          }
         </section>
+
+        { !isItemLoaded &&
+          <div className="text-center">
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        } 
+
+        
+
+        
       </section>
     </>
   )
--- v2/frontend/src/components/Item/ItemMain.jsx
+++ v2/frontend/src/components/Item/ItemMain.jsx
@@ -1,84 +1,99 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import ItemCondition from "./ItemCondition";
 import ItemList from "./ItemList";
 import axios from "axios";
+import { current } from "@reduxjs/toolkit";
 
 export default function ItemMain() {
 
   const [itemName, setItemName] = useState('');
-  const [job, setJob] = useState(null);
+  const [job, setJob] = useState("COMMON");
   const [minLevel, setMinLevel] = useState(0);
-  const [category, setCategory] = useState(null)
+  const [category, setCategory] = useState("NONE")
 
   const [itemList, setItemList] = useState([]);  
   const [isItemLoaded, setIsItemLoaded] = useState(false);
 
+  const isLoading = useRef(false);
+  // const searchCondition = useRef(DEFAULT_ITEM_CONDITION);
+  const nextPage = useRef(0);
+  const hasNextPage = useRef(false);
 
   // 화면에 접속하자마자 검색이 필요하다.
   useEffect(() => {
-    // console.log(`아이템 첫 로딩`)
+    console.log(`아이템 첫 로딩`)
+    isLoading.current = true;
     axios
-      .get('/api/items')
+      .get('/api/items?page=0&size=50')
       .then((res) => {
-        
         setItemList(res.data.items); // 조회 결과 아이템
-        // console.log(res.data.items)
         setIsItemLoaded(true);
+
+        if (res.data.items.length < 50) hasNextPage.current = false;
+        else hasNextPage.current = true;
+
+        nextPage.current += 1
+        isLoading.current = false;
+
+        console.log(res)
       })
       .catch((err) => {
-        // console.log(err);
+        console.log(err)
         setItemList([]); // 에러 발생시 아무런 아이템도 나타내지 않는다.
       });
-    }, []);
+  }, []);
 
   const handleItemName = (e) => {
-    setItemName(e.target.value)
-    // console.log(itemName);
+    setItemName(e.target.value);
   }
 
   const handleJob = (e) => {
-    console.log(e.target.value);
-    setJob(e.target.value)  
-    // console.log(`job = ${job}`)
+    setJob(e.target.value)
   }
 
   const handleMinLevel = (e) => {
-    setMinLevel(e.target.value);
-    // console.log(`new min level = ${minLevel}`)
+    setMinLevel(e.target.value)
   }
 
   const handleCategory = (e) => {
-    console.log(e.target.value);
     setCategory(e.target.value);
-    // console.log(`category change = ${category}`);
   }
 
-  const handleSubmitButton = (e) => {
-    e.preventDefault();
-    setIsItemLoaded(false);
-    const submitForm = {
+  function getSearchCondition() {
+    return {
       name: itemName,
       job: job === "NONE" ? null : job,
       minLevel: minLevel,
       category: category === "NONE" ? null : category
     }
+  }
+
+  function handleConditionSearch() {
+
+    console.log(`검색 버튼이 눌렸다.`)
+    console.log(`/api/items?page=${nextPage.current}&size=50`)
+    console.log(`searchCondition=${getSearchCondition()}`);
+    console.log(`hasNextPage=${hasNextPage.current}`)
+    console.log(`isLoading=${isLoading.current}`)
+
+    isLoading.current = true;
 
     axios
-      .post('/api/items', submitForm)
-      .then((res) => {
-        console.log(res)
-        // res에서 데이터를 가져온다.
-        setItemList(res.data.items)
-        setIsItemLoaded(true);
-        console.log(`itemList=${itemList}`)
-      })
-      .catch((err) => {
-        console.log(err)
-        setItemList([]);
-      })
+    .post('/api/items?page=0&size=50', getSearchCondition())
+    .then((res) => {
+      console.log(res)
+      setItemList(...res.data.items)
+      setIsItemLoaded(true); // 아이템 로딩 완료 => 스피너 사라짐
 
-    console.log(submitForm);
+      isLoading.current = false;
+      nextPage.current += 1
+      if (res.data.items.length < 50) hasNextPage.current = false;
+    })
+    .catch((err) => {
+      console.log(err)
+      setItemList([]);
+    })
   }
 
   return (
@@ -93,18 +108,21 @@ export default function ItemMain() {
                 minLevel={minLevel}
                 handleMinLevel={handleMinLevel} 
                 handleCategory={handleCategory}
-                handleSubmitButton={handleSubmitButton}
+                handleSubmitButton={handleConditionSearch}
               />
             </section>
-          </div>
+          </div>  
 
 
           <div className="col-lg-12 col-xl-8">
             <section>
-              <ItemList itemList={itemList} isItemLoaded={isItemLoaded}/>
+              <ItemList 
+                itemList={itemList} 
+                isItemLoaded={isItemLoaded}
+                hasNextPage={hasNextPage}
+              />
             </section>
           </div>
-
         </div>
       </section>
 
