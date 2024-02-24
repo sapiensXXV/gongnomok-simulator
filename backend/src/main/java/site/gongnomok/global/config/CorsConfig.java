98d59a2f64ecb56106e1022a511978c6484fbbc2
Jaehoon-So <jhspacelover@naver.com>
2024-02-24 21:17:38 +0900

    fix: fix base uri

---
+++ backend/src/main/java/site/gongnomok/global/config/CorsConfig.java
@@ -0,0 +1,2 @@
+package site.gongnomok.global.config;public class CorsConfig {
+}
--- frontend/src/global/uri.js
+++ frontend/src/global/uri.js
@@ -1,1 +1,1 @@
-export const BASE_URI = "http://localhost:8080"
+export const BASE_URI = "http://34.64.91.129:8080"
