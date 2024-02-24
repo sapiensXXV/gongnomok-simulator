4abedf6501ef26d9f5e676939d8019cc187d494b
Jaehoon-So <jhspacelover@naver.com>
2024-02-25 07:50:07 +0900

    fix: gongnomok.site CORS 해결

--- backend/src/main/java/site/gongnomok/global/config/CorsConfig.java
+++ backend/src/main/java/site/gongnomok/global/config/CorsConfig.java
@@ -18,7 +18,9 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://localhost:4173",
                         "http://localhost:8080",
                         "http://34.64.91.129",
-                        "http://34.64.91.129:80"
+                        "http://34.64.91.129:80",
+                        "http://gongnomok.site",
+                        "http://www.gongnomok.site"
                 )
                 .allowCredentials(true)
                 .maxAge(3000);
