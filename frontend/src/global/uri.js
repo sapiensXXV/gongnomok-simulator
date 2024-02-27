72e6808bad92248d1e4b01587a6fb7f35535e5d7
Jaehoon-So <jhspacelover@naver.com>
2024-02-28 07:15:17 +0900

    feat: fix baseuri

--- frontend/src/global/uri.js
+++ frontend/src/global/uri.js
@@ -1,6 +1,7 @@
 // export const BASE_URI = "http://34.64.91.129:8080"
 // export const BASE_URI = "http://localhost:8080"
-export const BASE_URI = "https://gongnomok.site:8080"
+export const BASE_URI = "//gongnomok.site:8080"
+// export const BASE_URI = "https://gongnomok.site:8080"
 // export const BASE_URI = "https://www.gongnomok.site:8080"
 
 
