2ecae7c591189294e492a82e6155f49718ee662d
Jaehoon-So <jhspacelover@naver.com>
2024-03-10 19:31:45 +0900

    add: timezone

--- backend/src/main/java/site/gongnomok/GongnomokApplication.java
+++ backend/src/main/java/site/gongnomok/GongnomokApplication.java
@@ -1,8 +1,11 @@
 package site.gongnomok;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,4 +15,11 @@ public class GongnomokApplication {
 		SpringApplication.run(GongnomokApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 }
+
+
