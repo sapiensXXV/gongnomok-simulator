e5c4ff37dc118b77c1fd10649d7691618763ae7c
Jaehoon-So <jhspacelover@naver.com>
2024-03-20 00:35:59 +0900

    add: 카테고리 예외

---
+++ backend/src/main/java/site/gongnomok/global/exception/CategoryException.java
@@ -0,0 +1,15 @@
+package site.gongnomok.global.exception;
+
+
+import lombok.Getter;
+
+@Getter
+public class CategoryException extends RuntimeException{
+    private final int code;
+    private final String message;
+
+    public CategoryException(ExceptionCode exceptionCode) {
+        this.code = exceptionCode.getCode();
+        this.message = exceptionCode.getMessage();
+    }
+}
