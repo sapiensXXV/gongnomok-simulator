b7976b3d953f4ea49024eb9c7b9c5ba82ab68ebf
Jaehoon-So <jhspacelover@naver.com>
2024-03-22 21:34:26 +0900

    fix: banner text

--- frontend/src/components/banner/InformBanner.jsx
+++ frontend/src/components/banner/InformBanner.jsx
@@ -2,7 +2,7 @@ export default function InformBanner() {
   return (
     <>
       <div className="inform-banner-root">
-        현재 등록된 기록은 점수산정 로직 업데이트 후 <b>초기화 예정</b>입니다
+        점수산정 로직 업데이트 후 기록이 초기화되었습니다
       </div>
     </>
   )
