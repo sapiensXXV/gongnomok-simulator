5a43d16ed016a7fd1584a92db8011d51408c05ac
Jaehoon-So <jhspacelover@naver.com>
2024-04-14 04:50:25 +0900

    fix: request backend uri

--- frontend/src/global/uri.js
+++ frontend/src/global/uri.js
@@ -1,3 +1,3 @@
-export const BASE_URI = "http://localhost:8080" // 로컬 HTTP주소
-// export const BASE_URI = "https://gongnomok.site"
+// export const BASE_URI = "http://localhost:8080" // 로컬 HTTP주소
+export const BASE_URI = "https://gongnomok.site"
 // export const BASE_URI = "http://35.216.38.200"
