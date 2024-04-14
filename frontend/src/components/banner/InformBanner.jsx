47bcceaf3233ee5ea1c29a65542c1a79763c8436
Jaehoon-So <jhspacelover@naver.com>
2024-04-14 18:16:17 +0900

    fix: 배너 텍스트 수정

--- frontend/src/components/banner/InformBanner.jsx
+++ frontend/src/components/banner/InformBanner.jsx
@@ -2,7 +2,8 @@ export default function InformBanner() {
   return (
     <>
       <div className="inform-banner-root">
-        마법사 방어구가 추가되었습니다
+        <div>도적 모자, 장갑이 추가되었습니다</div>
+        <div>인기 아이템 목록이 초기화되었습니다</div>
       </div>
     </>
   )
