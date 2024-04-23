6eda3f22ee9112036d4388bf66b9de162515853e
Jaehoon-So <jhspacelover@naver.com>
2024-04-23 17:55:27 +0900

    delete: pinpoint

--- install-dev.sh
+++ install-dev.sh
@@ -4,6 +4,7 @@ cd backend
 ./gradlew clean build
 cd ..
 
+docker compose -f docker-compose-dev.yml rm
 docker compose -f docker-compose-dev.yml build --no-cache
 docker compose -f docker-compose-dev.yml push
 
