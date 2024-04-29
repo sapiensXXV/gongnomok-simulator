5432aefbfe20ea8f6f684ddf1b7d4b3021761983
Jaehoon-So <jhspacelover@naver.com>
2024-04-30 04:50:10 +0900

    add: add Nginx at backend server

---
+++ backend/script/run-dev.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+nginx -g daemon off
+
+java -jar \
+-javaagent:/backend/pinpoint-agent-2.5.4/pinpoint-bootstrap-2.5.4.jar \
+-Dpinpoint.applicationName=gongnomok-dev-2 \
+-Dpinpoint.config=/backend/pinpoint-agent-2.5.4/pinpoint-root.config \
+-Dspring.profiles.active=dev \
+app.jar
