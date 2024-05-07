77dd2fd1e9d8e5b1cbc0e1133e2e088bbaa1029f
Jaehoon-So <jhspacelover@naver.com>
2024-05-08 03:25:32 +0900

    fix: 배너 내용 수정

--- frontend/src/components/banner/InformBanner.jsx
+++ frontend/src/components/banner/InformBanner.jsx
@@ -2,8 +2,7 @@ export default function InformBanner() {
   return (
     <>
       <div className="inform-banner-root">
-        <div>도적 모자, 장갑이 추가되었습니다</div>
-        <div>인기 아이템 목록이 초기화되었습니다</div>
+        <div>도적 - <b>상의, 하의, 한벌옷, 신발</b> 추가</div>
       </div>
     </>
   )
