b08ec0fade47ca6bb30fca7be7ee5c581a71c86e
Jaehoon-So <jhspacelover@naver.com>
2024-05-13 18:59:34 +0900

    fix: title css

--- frontend/src/components/banner/InformBanner.jsx
+++ frontend/src/components/banner/InformBanner.jsx
@@ -3,6 +3,7 @@ export default function InformBanner() {
     <>
       <div className="inform-banner-root">
         <div>도적 - <b>상의, 하의, 한벌옷, 신발</b> 추가</div>
+        <div>5월 15일 기록 초기화 예정입니다</div>
       </div>
     </>
   )
