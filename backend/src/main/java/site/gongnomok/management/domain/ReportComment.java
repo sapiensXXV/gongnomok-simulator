4dd1aede8f32acb27f48c7e04ca6f52a95acf9d0
Jaehoon-So <jhspacelover@naver.com>
2024-05-15 21:01:16 +0900

    add: 댓글 신고 Entity

---
+++ backend/src/main/java/site/gongnomok/management/domain/ReportComment.java
@@ -0,0 +1,27 @@
+package site.gongnomok.management.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.gongnomok.comment.domain.Comment;
+
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class ReportComment {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_report_id")
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @Column(name = "comment_id")
+    private Comment commentId;
+
+    private int count;
+}
