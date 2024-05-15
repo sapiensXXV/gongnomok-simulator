47a8db953a2320a47184593910834e135938221f
Jaehoon-So <jhspacelover@naver.com>
2024-05-15 18:56:40 +0900

    fix: 배너 내용 수정

--- frontend/src/components/banner/InformBanner.jsx
+++ frontend/src/components/banner/InformBanner.jsx
@@ -2,8 +2,7 @@ export default function InformBanner() {
   return (
     <>
       <div className="inform-banner-root">
-        <div>도적 - <b>상의, 하의, 한벌옷, 신발</b> 추가</div>
-        <div>5월 15일 기록 초기화 예정입니다</div>
+        <div>5/15 ~ 최고기록이 초기화 되었습니다</div>
       </div>
     </>
   )
