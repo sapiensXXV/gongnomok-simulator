3d6e0d547051b4a393a911750268a0ff2b4a5b24
Jaehoon-So <jhspacelover@naver.com>
2024-05-18 22:53:47 +0900

    fix: fix pageable pacakge to spring data

--- backend/src/main/java/site/gongnomok/management/domain/repository/ReportCommentJpaRepository.java
+++ backend/src/main/java/site/gongnomok/management/domain/repository/ReportCommentJpaRepository.java
@@ -1,10 +1,10 @@
 package site.gongnomok.management.domain.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.gongnomok.management.domain.ReportComment;
 import site.gongnomok.management.dto.response.ReportCommentDto;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface ReportCommentJpaRepository extends JpaRepository<ReportComment, Long>, ReportCommentQueryRepository {
