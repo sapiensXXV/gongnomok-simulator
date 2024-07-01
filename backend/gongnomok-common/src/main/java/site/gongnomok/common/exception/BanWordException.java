2a5b70d8ce45f6f9b91ad16a35c88b0a49fbeb53
Jaehoon-So <jhspacelover@naver.com>
2024-07-01 11:59:06 +0900

    feat: 금칙어 예외

--- backend/gongnomok-common/src/main/java/site/gongnomok/common/exception/BanWordException.java
+++ backend/gongnomok-common/src/main/java/site/gongnomok/common/exception/BanWordException.java
@@ -1,2 +1,12 @@
-package site.gongnomok.common.exception;public class BanWordException {
+package site.gongnomok.common.exception;
+
+public class BanWordException extends RuntimeException {
+
+    private final int code;
+    private final String message;
+
+    public BanWordException(ExceptionCode exceptionCode) {
+        this.code = exceptionCode.getCode();
+        this.message = exceptionCode.getMessage();
+    }
 }
