f7edc05d36d3a8006daf370df6fa765567a5fb95
Jaehoon-So <jhspacelover@naver.com>
2024-08-11 05:00:01 +0900

    delete: typescript directory

--- frontend-typescript/src/contexts/LoginState.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from "recoil"
-import { persistAtom } from "./persistAtom"
-
-const LOGIN_STATE = "LOGIN_STATE"
-export const LoginState = atom({
-  key: LOGIN_STATE,
-  default: false,
-  effects_UNSTABLE: [persistAtom]
-})
