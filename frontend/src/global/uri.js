4df21aecc985d4e477339cbc620e72f515b5ae36
Jaehoon-So <jhspacelover@naver.com>
2024-08-12 18:46:54 +0900

    chore: little change

--- frontend/src/global/uri.js
+++ frontend/src/global/uri.js
@@ -3,4 +3,3 @@
 export const BASE_URI = "https://dev.gongnomok.site" // 개발 서버 주소
 // export const BASE_URI = "http://35.216.38.200"
 // export const BASE_URI = "http://35.216.73.167:8080"
-// change
