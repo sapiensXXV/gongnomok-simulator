c8101f8a96fb190062c6affb46ff275fcf7e6549
Jaehoon-So <jhspacelover@naver.com>
2024-08-13 18:34:41 +0900

    fix: 도커 컴포즈 수정

--- frontend/src/components/banner/InformBanner.jsx
+++ frontend/src/components/banner/InformBanner.jsx
@@ -2,7 +2,8 @@ export default function InformBanner() {
   return (
     <>
       <div className="inform-banner-root banner">
-        <div>7/14 ~ 기록 초기화</div>
+        <div>8/14 ~ 기록 초기화</div>
+        <div>궁수 아이템 추가 완료</div>
       </div>
     </>
   )
