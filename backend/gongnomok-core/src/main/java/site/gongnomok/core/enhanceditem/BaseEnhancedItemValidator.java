32dd2f367827a1024923955a20a78632eb4a88ed
Jaehoon-So <jhspacelover@naver.com>
2024-08-13 16:40:06 +0900

    feat: 등록 요청 검증로직

--- backend/gongnomok-core/src/main/java/site/gongnomok/core/enhanceditem/BaseEnhancedItemValidator.java
+++ backend/gongnomok-core/src/main/java/site/gongnomok/core/enhanceditem/BaseEnhancedItemValidator.java
@@ -8,17 +8,15 @@ import site.gongnomok.common.enhanceditem.dto.request.ItemEnhanceServiceRequest;
 import site.gongnomok.common.exception.EnhancedItemException;
 import site.gongnomok.common.exception.ExceptionCode;
 import site.gongnomok.common.exception.ItemException;
+import site.gongnomok.core.scroll.ItemStat;
 import site.gongnomok.core.scroll.Scroll;
-import site.gongnomok.data.enhanceditem.domain.EnhanceScroll;
 import site.gongnomok.data.item.domain.Item;
 import site.gongnomok.data.item.domain.repository.ItemRepository;
 
 import java.util.Map;
 
-import static site.gongnomok.common.exception.ExceptionCode.INVALID_ENHANCED_SCORE_REQUEST;
 import static site.gongnomok.common.exception.ExceptionCode.INVALID_ENHANCED_SUCCESS_REQUEST;
 import static site.gongnomok.core.scroll.ItemStat.*;
-import static site.gongnomok.core.scroll.Scroll.isEqualWith;
 
 /**
  * 강화 아이템 요청을 검증하는 책임을 가지는 클래스
@@ -38,12 +36,11 @@ public class BaseEnhancedItemValidator implements EnhanceItemValidator {
     ) {
         Item findItem = itemRepository.findById(itemId)
             .orElseThrow(() -> new ItemException(ExceptionCode.NOT_FOUND_ITEM_ID));
-
-        validateSuccessCount(request.getSuccess(), findItem.getUpgradable());
-        validateScore(request.getSuccess(), request.getScroll(), request.getUpgradable());
-//        validateUpgradable(request.getUpgradable(), findItem.getUpgradable());
-//        validateUpgradeStatus(request.getStatus(), request.getSuccess(), request.getScroll());
+        
+        validateSuccessCount(request.getSuccess(), findItem.getUpgradable()); // 성공횟수 검사
+        validateStatus(request.getScroll(), request.getStatus(), request.getSuccess()); // 능력치 상태검사
     }
+
 
     /**
      * 강화 성공 횟수의 유효성을 검증합니다.
@@ -52,79 +49,94 @@ public class BaseEnhancedItemValidator implements EnhanceItemValidator {
      * @param successDto 강화 성공 정보를 담고 있는 DTO
      * @throws EnhancedItemException 총 성공 횟수가 10을 초과하는 경우
      */
-    private void validateSuccessCount(final EnhanceSuccessDto successDto, int mxSuccessCnt) {
+    private void validateSuccessCount(final EnhanceSuccessDto successDto, int itemUpgradable) {
         final int ten = successDto.getTen();
         final int sixty = successDto.getSixty();
         final int hundred = successDto.getHundred();
 
-        final int total = ten + sixty + hundred;
-        if (total > mxSuccessCnt) {
+        final int sumOfScrollSuccess = ten + sixty + hundred;
+        final int totalSuccessAtRequest = successDto.getTotal();
+        if (sumOfScrollSuccess > itemUpgradable || totalSuccessAtRequest > itemUpgradable) {
             throw new EnhancedItemException(INVALID_ENHANCED_SUCCESS_REQUEST);
         }
     }
-
-    /**
-     * 강화 점수의 유효성을 검증합니다.
-     * 실제 강화 점수가 주어진 스크롤과 강화 가능 횟수에 따른 최대 점수를 초과하면 안 됩니다.
-     *
-     * @param success 강화 성공 정보를 담고 있는 DTO
-     * @param scrollName 사용된 강화 스크롤의 이름
-     * @param upgradable 아이템의 강화 가능 횟수
-     * @throws EnhancedItemException 실제 강화 점수가 최대 허용 점수를 초과하는 경우
-     */
-    private void validateScore(
-        final EnhanceSuccessDto success,
+    
+    private void validateStatus (
         final String scrollName,
-        final int upgradable
-    ) {
-        EnhanceScroll scroll = EnhanceScroll.from(scrollName);
-        final int maximumScore = scroll.getMaximumScore(upgradable);
-        final int actualScore = EnhanceScroll.calculateScore(success, scrollName);
-        if (actualScore > maximumScore) {
-            throw new EnhancedItemException(INVALID_ENHANCED_SCORE_REQUEST);
-        }
-    }
-
-    /**
-     * 아이템의 강화 가능 횟수를 검증합니다.
-     *
-     * @param upgradable 기대되는 강화 가능 횟수
-     * @param mxUpgradable 아이템의 최대 강화 횟수
-     * @throws ItemException 아이템 ID에 해당하는 아이템을 찾을 수 없는 경우
-     * @throws EnhancedItemException 아이템의 실제 강화 가능 횟수가 기대값과 다른 경우
-     */
-    private void validateUpgradable(
-        final int upgradable,
-        final int mxUpgradable
-    ) {
-        if (mxUpgradable != upgradable) {
-            throw new EnhancedItemException(ExceptionCode.INVALID_UPGRADABLE_COUNT);
-        }
-    }
-
-    /**
-     * 아이템의 강화 수치를 검증합니다.
-     */
-    private void validateUpgradeStatus(
         final EnhanceStatusDto status,
-        final EnhanceSuccessDto success,
-        final String scroll
-    ) {
-        Map<Integer, Scroll> scrollMap = Scroll.toProbabilityMapFrom(scroll);
-        isEqualWith(STR, status.getStr(), scrollMap, success);
-        isEqualWith(DEX, status.getDex(), scrollMap, success);
-        isEqualWith(INT, status.getAcc(), scrollMap, success);
-        isEqualWith(LUK, status.getLuk(), scrollMap, success);
-        isEqualWith(PHY_ATK, status.getPhyAtk(), scrollMap, success);
-        isEqualWith(MG_ATK, status.getMgAtk(), scrollMap, success);
-        isEqualWith(PHY_DEF, status.getPhyDef(), scrollMap, success);
-        isEqualWith(MG_DEF, status.getMgDef(), scrollMap, success);
-        isEqualWith(ACC, status.getAcc(), scrollMap, success);
-        isEqualWith(AVO, status.getAvo(), scrollMap, success);
-        isEqualWith(MOVE, status.getMove(), scrollMap, success);
-        isEqualWith(JUMP, status.getJump(), scrollMap, success);
-        isEqualWith(HP, status.getHp(), scrollMap, success);
-        isEqualWith(MP, status.getMp(), scrollMap, success);
+        final EnhanceSuccessDto success) {
+        
+        // 사용자가 사용한 주문서 정보를 바탕으로 실제 강화 내용과 일치하는지 검사한다.
+        Map<Integer, Scroll> scrolls = Scroll.findScrollFrom(scrollName);
+        
+        // str 결과 계산
+        if (!matchStatusValue(success, status, scrolls)) {
+            throw new EnhancedItemException(ExceptionCode.INVALID_UPGRADED_STATUS); 
+        }
     }
+    
+    private boolean matchStatusValue(
+        final EnhanceSuccessDto success,
+        final EnhanceStatusDto status,
+        final Map<Integer, Scroll> scrolls
+    ) {
+        if (status.getStr() != calculateEnhanceStatus(success, STR, scrolls)) return false;
+        if (status.getDex() != calculateEnhanceStatus(success, DEX, scrolls)) return false;
+        if (status.getIntel() != calculateEnhanceStatus(success, INT, scrolls)) return false;
+        if (status.getLuk() != calculateEnhanceStatus(success, LUK, scrolls)) return false;
+        if (status.getPhyAtk() != calculateEnhanceStatus(success, PHY_ATK, scrolls)) return false;
+        if (status.getPhyDef() != calculateEnhanceStatus(success, PHY_DEF, scrolls)) return false;
+        if (status.getMgAtk() != calculateEnhanceStatus(success, MG_ATK, scrolls)) return false;
+        if (status.getMgDef() != calculateEnhanceStatus(success, MG_DEF, scrolls)) return false;
+        if (status.getMove() != calculateEnhanceStatus(success, MOVE, scrolls)) return false;
+        if (status.getJump() != calculateEnhanceStatus(success, JUMP, scrolls)) return false;
+        if (status.getAcc() != calculateEnhanceStatus(success, ACC, scrolls)) return false;
+        if (status.getAvo() != calculateEnhanceStatus(success, AVO, scrolls)) return false;
+        if (status.getHp() != calculateEnhanceStatus(success, HP, scrolls)) return false;
+        if (status.getMp() != calculateEnhanceStatus(success, MP, scrolls)) return false;
+        
+        return true;
+    }
+    
+    private int calculateEnhanceStatus(
+        final EnhanceSuccessDto success,
+        final ItemStat stat,
+        final Map<Integer, Scroll> scrolls
+        ) {
+        Scroll tenPerScroll = scrolls.get(10);
+        Scroll sixtyPerScroll = scrolls.get(60);
+        Scroll hundredPerScroll = scrolls.get(100);
 
+        return switch (stat) {
+            case STR ->
+                tenPerScroll.getStr() * success.getTen() + sixtyPerScroll.getStr() * success.getSixty() + hundredPerScroll.getStr() * success.getHundred();
+            case DEX ->
+                tenPerScroll.getDex() * success.getTen() + sixtyPerScroll.getDex() * success.getSixty() + hundredPerScroll.getDex() * success.getHundred();
+            case INT ->
+                tenPerScroll.getIntel() * success.getTen() + sixtyPerScroll.getIntel() * success.getSixty() + hundredPerScroll.getIntel() * success.getHundred();
+            case LUK ->
+                tenPerScroll.getLuk() * success.getTen() + sixtyPerScroll.getLuk() * success.getSixty() + hundredPerScroll.getLuk() * success.getHundred();
+            case PHY_ATK ->
+                tenPerScroll.getPhyAtk() * success.getTen() + sixtyPerScroll.getPhyAtk() * success.getSixty() + hundredPerScroll.getPhyAtk() * success.getHundred();
+            case PHY_DEF ->
+                tenPerScroll.getPhyDef() * success.getTen() + sixtyPerScroll.getPhyDef() * success.getSixty() + hundredPerScroll.getPhyDef() * success.getHundred();
+            case MG_ATK ->
+                tenPerScroll.getMgAtk() * success.getTen() + sixtyPerScroll.getMgAtk() * success.getSixty() + hundredPerScroll.getMgAtk() * success.getHundred();
+            case MG_DEF ->
+                tenPerScroll.getMgDef() * success.getTen() + sixtyPerScroll.getMgDef() * success.getSixty() + hundredPerScroll.getMgDef() * success.getHundred();
+            case ACC ->
+                tenPerScroll.getAcc() * success.getTen() + sixtyPerScroll.getAcc() * success.getSixty() + hundredPerScroll.getAcc() * success.getHundred();
+            case AVO ->
+                tenPerScroll.getAvo() * success.getTen() + sixtyPerScroll.getAvo() * success.getSixty() + hundredPerScroll.getAvo() * success.getHundred();
+            case MOVE ->
+                tenPerScroll.getMove() * success.getTen() + sixtyPerScroll.getMove() * success.getSixty() + hundredPerScroll.getMove() * success.getHundred();
+            case JUMP ->
+                tenPerScroll.getJump() * success.getTen() + sixtyPerScroll.getJump() * success.getSixty() + hundredPerScroll.getJump() * success.getHundred();
+            case HP ->
+                tenPerScroll.getHp() * success.getTen() + sixtyPerScroll.getHp() * success.getSixty() + hundredPerScroll.getHp() * success.getHundred();
+            case MP ->
+                tenPerScroll.getMp() * success.getTen() + sixtyPerScroll.getMp() * success.getSixty() + hundredPerScroll.getMp() * success.getHundred();
+        };
+    }
+    
 }
