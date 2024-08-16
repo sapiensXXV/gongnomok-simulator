91f44fb1d5dc44df41faf163d74a531b40922d63
Jaehoon-So <jhspacelover@naver.com>
2024-08-17 02:42:37 +0900

    fix: css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,20 +12,30 @@ import '../css/common.css'
 import '../css/font.css'
 import axios from 'axios'
 
+import styles from './main.module.css'
+
 axios.defaults.withCredentials = true
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
 
-    <div className='row gongnomok-global'>
+{/*    <div className={`row gongnomok-global ${styles.main_grid} `}>
       <div className='col col-lg-0 col-xxl-2'></div>
       <div className='col-lg-12 col-xxl-8'>
 
-        <App />
+        <App/>
 
       </div>
       <div className='col-lg-0 col-xxl-2'></div>
-    </div>
+    </div>*/}
+
+    <main className={styles.main_grid}>
+      <div className={styles.dummy_block}/>
+      <div className=''>
+        <App/>
+      </div>
+      <div className={styles.dummy_block}/>
+    </main>
 
 
   </BrowserRouter>
