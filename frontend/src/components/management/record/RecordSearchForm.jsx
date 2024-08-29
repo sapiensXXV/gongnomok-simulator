199cfe301e0fd34f45188b4099bd7c241fb3259f
Jaehoon-So <jhspacelover@naver.com>
2024-08-29 12:50:35 +0900

    add: 기록 클리너 버튼 핸들러

--- frontend/src/components/management/record/RecordSearchForm.jsx
+++ frontend/src/components/management/record/RecordSearchForm.jsx
@@ -3,14 +3,15 @@ import styles from './RecordSearchForm.module.css';
 // eslint-disable-next-line react/prop-types
 function RecordSearchForm({
   recordSearch,
-  inputChange
+  inputChange,
+  cleanRecord
 }) {
   return (
     <>
       <main className={styles.record_search_container}>
         <input placeholder='아이템 이름' onChange={inputChange}/>
         <button className={styles.item_search_btn} onClick={recordSearch}>검색</button>
-        <button className={styles.all_clean_btn}>전체 클리너</button>
+        <button className={styles.all_clean_btn} onClick={cleanRecord} >전체 클리너</button>
       </main>
     </>
   )
