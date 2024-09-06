76201f861b1a0bd685606d679c3aa732fccbc006
Jaehoon-So <jhspacelover@naver.com>
2024-09-06 19:42:22 +0900

    feat: 레플리카 적용

--- backend/gongnomok-data/src/main/java/site/gongnomok/data/configure/DatasourceConfig.java
+++ backend/gongnomok-data/src/main/java/site/gongnomok/data/configure/DatasourceConfig.java
@@ -2,20 +2,31 @@ package site.gongnomok.data.configure;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import site.gongnomok.data.infrastructure.RoutingDataSource;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
 
 @Configuration
 @Slf4j
 public class DatasourceConfig {
 
+    private static final String SOURCE_SERVER = "SOURCE";
+    private static final String REPLICA_SERVER1 = "REPLICA1";
+    private static final String REPLICA_SERVER2 = "REPLICA2";
+    
     @Bean
     @Profile("test")
-    public DataSource dataSource() {
+    public DataSource testDataSource() {
         log.info("테스트 데이터소스 초기화");
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
@@ -24,4 +35,60 @@ public class DatasourceConfig {
         dataSource.setPassword("");
         return dataSource;
     }
+    
+    @Bean
+    @Qualifier(SOURCE_SERVER)
+    @ConfigurationProperties(prefix = "spring.datasource.source")
+    public DataSource sourceDataSource() {
+        return DataSourceBuilder.create()
+            .build();
+    }
+    
+    @Bean
+    @Qualifier(REPLICA_SERVER1)
+    @ConfigurationProperties(prefix = "spring.datasource.replica1")
+    public DataSource replicaDataSourceOne() {
+        return DataSourceBuilder.create()
+            .build();
+    }
+    
+    @Bean
+    @Qualifier(REPLICA_SERVER2)
+    @ConfigurationProperties(prefix = "spring.datasource.replica2")
+    public DataSource replicaDataSourceTwo() {
+        return DataSourceBuilder.create()
+            .build();
+    }
+    
+    @Bean
+    public DataSource routingDataSource(
+        @Qualifier(SOURCE_SERVER) DataSource sourceDataSource,
+        @Qualifier(REPLICA_SERVER1) DataSource replicaDataSourceOne,
+        @Qualifier(REPLICA_SERVER2) DataSource replicaDataSourceTwo
+    ) {
+        RoutingDataSource routingDataSource = new RoutingDataSource();
+
+        HashMap<Object, Object> dataSourceMap = new HashMap<>();
+        dataSourceMap.put("source", sourceDataSource);
+        dataSourceMap.put("replica1", replicaDataSourceOne);
+        dataSourceMap.put("replica2", replicaDataSourceTwo);
+        
+        routingDataSource.setTargetDataSources(dataSourceMap);
+        routingDataSource.setDefaultTargetDataSource(sourceDataSource);
+
+        return routingDataSource;
+    } 
+    
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        DataSource determinedDataSource = routingDataSource(
+            sourceDataSource(), 
+            replicaDataSourceOne(), 
+            replicaDataSourceTwo()
+        );
+        return new LazyConnectionDataSourceProxy(determinedDataSource);
+    }
+    
+    
 }
--- backend/gongnomok-data/src/main/java/site/gongnomok/data/infrastructure/RoutingDataSource.java
+++ backend/gongnomok-data/src/main/java/site/gongnomok/data/infrastructure/RoutingDataSource.java
@@ -1,2 +1,30 @@
-package site.gongnomok.data.infrastructure;public class RoutingDataSource {
+package site.gongnomok.data.infrastructure;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+
+@Slf4j
+public class RoutingDataSource extends AbstractRoutingDataSource {
+    
+    private AtomicLong counter = new AtomicLong(0);
+    
+    @Override
+    protected Object determineCurrentLookupKey() {
+        boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+        if (isReadOnly) {
+            if (counter.get() == Long.MAX_VALUE) {
+                counter = new AtomicLong(0);
+            }
+            if (counter.getAndIncrement() % 2 == 0) {
+                return "replica1";
+            } else {
+                return "replica2";
+            }
+        }
+        return "source";
+    }
 }
