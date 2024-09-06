f115f1fc8afcbc77ef342f3d502d9bb2bba85989
Jaehoon-So <jhspacelover@naver.com>
2024-09-06 21:31:57 +0900

    feat: 레플리카 서버 접근 RR 개선

--- backend/gongnomok-data/src/main/java/site/gongnomok/data/infrastructure/RoutingDataSource.java
+++ backend/gongnomok-data/src/main/java/site/gongnomok/data/infrastructure/RoutingDataSource.java
@@ -4,26 +4,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 @Slf4j
 public class RoutingDataSource extends AbstractRoutingDataSource {
-    
-    private AtomicLong counter = new AtomicLong(0);
+
+    private final List<String> replicaKeys = List.of("replica1", "replica2");
+    private final AtomicInteger counter = new AtomicInteger(0); 
     
     @Override
     protected Object determineCurrentLookupKey() {
         boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
         if (isReadOnly) {
-            if (counter.get() == Long.MAX_VALUE) {
-                counter = new AtomicLong(0);
-            }
-            if (counter.getAndIncrement() % 2 == 0) {
-                return "replica1";
-            } else {
-                return "replica2";
-            }
+            int index = counter.getAndIncrement() % replicaKeys.size();
+            return replicaKeys.get(index);
         }
         return "source";
     }
