71d2ab4d9d43d42a82af4b4540fa84c9f8c6f337
Jaehoon-So <jhspacelover@naver.com>
2024-12-01 04:30:39 +0900

    fix: uri 추가

--- frontend/src/global/uri.js
+++ frontend/src/global/uri.js
@@ -1,3 +1,3 @@
 // 요청 URI
 export const BASE_URL = import.meta.env.VITE_REQUEST_API_URL;
-export const CDN_URL = import.meta.env.VITE_ASSETS_URL;
+export const ASSETS_URL = import.meta.env.VITE_ASSETS_URL;
