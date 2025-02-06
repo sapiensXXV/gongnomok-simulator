5b3ce44b351d67880beef87bebf88ced9ede12ae
Jaehoon-So <jhspacelover@naver.com>
2025-02-06 23:20:52 +0900

    fix: fix inform banner

--- frontend/src/components/banner/InformBanner.jsx
+++ frontend/src/components/banner/InformBanner.jsx
@@ -4,9 +4,7 @@ export default function InformBanner() {
   return (
     <>
       <div className={`${styles.container} ${styles.banner}`}>
-        {/*<div>8/14 ~ 기록 초기화</div>*/}
-        {/*<div>8/14 ~ 인기 아이템 초기화</div>*/}
-        <div>1/3 ~ 기록이 초기화되었습니다</div>
+        <div>2/6 ~ 기록이 초기화되었습니다</div>
       </div>
     </>
   )
